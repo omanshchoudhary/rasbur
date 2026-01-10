e8ca8e6ec45188a1fbb81528e03c65108873b403
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-01-10 15:53:35 +0530

    Add hexadecimal decoder with detection and ascii decoding

---
+++ backend/src/decoders/HexDecoder.js
@@ -0,0 +1,38 @@
+import Decoder from "./Decoder.js";
+
+export default class HexDecoder extends Decoder {
+    name = "Hexadecimal"
+
+    confidence(input) {
+        if (!input) return 0;
+
+        const cleanInput = input.replace(/\s+/g, '');
+
+        if (cleanInput.length % 2 !== 0) return 0
+
+        const hexRegex = /^[0-9a-fA-F]+$/;
+        return hexRegex.test(cleanInput) ? 0.75 : 0;
+    }
+
+    decode(input) {
+        try {
+            const cleanInput = input.replace(/\s+/g, '');
+            let result = '';
+
+            for (let i = 0; i < cleanInput.length; i+=2) {
+                const byte = parseInt(cleanInput.slice(i, i + 2), 16)
+                if (isNaN(byte)) return null;
+                result += String.fromCharCode(byte)
+            }
+            if (!result || result.includes('\u0000')) return null;
+
+            return result;
+        } catch {
+            return null
+        }
+    }
+
+    explain() {
+        return "Hexadecimal encoding represents binary data using base-16 values (0–9, A–F).";
+    }
+}
--- backend/src/registry/decoderRegistry.js
+++ backend/src/registry/decoderRegistry.js
@@ -1,11 +1,14 @@
 import BaseDecoder from "../decoders/Decoder.js"
 import Base64Decoder from "../decoders/Base64Decoder.js"
 import UrlDecoder from "../decoders/UrlDecoder.js";
+import HexDecoder from "../decoders/HexDecoder.js";
 
 const decoders = []
 
 registerDecoder(new Base64Decoder());
 registerDecoder(new UrlDecoder())
+registerDecoder(new HexDecoder())
+
 export function registerDecoder(decoder){
     if(!(decoder instanceof BaseDecoder)){
         throw new Error("Invalid Decoder")
