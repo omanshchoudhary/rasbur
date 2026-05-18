f9f3f710db2a4ac1a083b3cf98e2291f6af46149
Omansh Choudhary <omansh.choudhary@gmail.com>
2026-05-18 11:09:25 +0530

    feat(api): add user mongoose model

---
+++ apps/api/src/models/user.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+
+export const userSchema = new Schema(
+    {
+        name: { type: String, required: true, trim: true },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            unique: true,
+            index: true,
+        },
+        avatar: { type: String },
+        oauthProvider: { type: String, required: true },
+        oauthId: { type: String, required: true, index: true },
+        isActive: { type: Boolean, default: true },
+        tier: { type: String, enum: ['free', 'pro'], default: 'free' },
+        roles: { type: [String], default: [] },
+    },
+    {
+        timestamps: { createdAt: true, updatedAt: true },
+    }
+);
+
+export const User = mongoose.model('User', userSchema);
